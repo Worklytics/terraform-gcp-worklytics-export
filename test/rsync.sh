b5318b0a26faa2b664c59b77a53633269f2fa9fc
Erik Schultink <erik@worklytics.co>
2023-09-20 10:35:23 -0700

    remove unneeded command

--- test/rsync.sh
+++ test/rsync.sh
@@ -9,9 +9,6 @@ BUCKET_NAME=$2
 # https://cloud.google.com/sdk/gcloud/reference/auth/print-identity-token
 CI_RUN=`date +%Y%m%d'T'%H%M%S`
 
-# should work, assuming you're running this somewhere that your gcloud CLI is auth'd as something
-# that can impersonate the target service account
-GCP_TOKEN=`gcloud auth print-identity-token --impersonate-service-account=${EXAMPLE_TENANT_SA_EMAIL}`
 
 echo "EXAMPLE_TENANT_SA_EMAIL: ${EXAMPLE_TENANT_SA_EMAIL}"
 echo "BUCKET_NAME: ${BUCKET_NAME}"
