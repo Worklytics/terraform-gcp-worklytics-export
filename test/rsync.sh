9d58e83a6453186a9b1e22c1a79ecb728ff1f1d0
Erik Schultink <eschultink@users.noreply.github.com>
2024-08-20 13:07:45 -0700

    fix rc script

--- test/rsync.sh
+++ test/rsync.sh
@@ -14,4 +14,4 @@ echo "BUCKET_NAME: ${BUCKET_NAME}"
 # if bucket exists + perms OK, this should work
 mkdir /tmp/$CI_RUN
 echo "TEST" > /tmp/$CI_RUN/test.txt
-gsutil --impersonate-service-account=${EXAMPLE_TENANT_SA_EMAIL} -m rsync -r /tmp/$CI_RUN gs://$BUCKET_NAME
+gsutil --impersonate-service-account=${EXAMPLE_TENANT_SA_EMAIL} -m rsync -r /tmp/$CI_RUN gs://$BUCKET_NAME/$CI_RUN
